70ccad742e429c970c3a4471f10b9a5e4f96ba5d
fabiojcbatista <fabiojcbweb@gmail.com>
2021-11-09 20:14:38 -0300

    Client/ listagem de ofertas

---
+++ client/src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import './App.css';
+import { Navbar, Nav } from 'react-bootstrap';
+import { NavLink, Outlet} from 'react-router-dom';
+
+export default function App() {
+  return (
+    <>
+
+    
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand as={NavLink} to="/">Desafio</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={NavLink} to="/ofertas">Ofertas</Nav.Link>
+            <Nav.Link as={NavLink} to="/sobre">Sobre</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <header>
+        
+
+             
+
+
+      </header>
+      <main>
+        <Outlet />
+      </main>
+      <footer>
+        <p>Desenvolvido por Fabio J. C. Batista e Matheus G. M. da Silva</p>
+      </footer>
+    </>
+  );
+}
---
+++ client/src/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
+import App from './App';
+import PaginaNaoEcontrada from './PaginaNaoEcontrada';
+import PaginaPrincipal from './PaginaPrincipal';
+import PaginaOfertas from './PaginaOfertas';
+import PaginaSobre from './PaginaSobre';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<PaginaPrincipal />}/>
+          <Route path="ofertas" element={<PaginaOfertas />}>
+                     </Route>
+          <Route path="sobre" element={<PaginaSobre />} />
+          <Route path="*" element={<PaginaNaoEcontrada />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
